671651eb9a971191d52d685dbf90eb440ee11df6
Santi Otero <poyovcp@gmail.com>
2020-04-09 16:40:58 -0300

    app pwa

---
+++ js/sw_util.js
@@ -0,0 +1,19 @@
+
+
+function actualizaCacheDinamico(dynCache, req, res){
+
+	if( res.ok ){
+
+		caches.open(dynCache).then( cache => {
+
+			cache.put(req, res.clone());
+			return res.clone();
+
+		});
+
+	}else{
+			return res;
+	}
+
+
+}
---
+++ sw.js
@@ -0,0 +1,72 @@
+importScripts('js/sw_util.js');
+
+
+const STATIC_CACHE = 'static-v1';
+const DYNAMIC_CACHE = 'dynamic-v1';
+const INMUTABLE_CACHE = 'inmutable-v1';
+
+const APP_SHELL = [
+	'/',
+	'index.html',
+	'img/favicon.png',
+	'img/agradecimiento.png',
+	'img/alerta.png',
+	'img/compras.png',
+	'img/feliz.png',
+	'js/app.js',
+	'js/sw_util.js'
+];
+
+const APP_SHELL_INMUT = [
+	'css/materialize.min.css',
+	'https://fonts.googleapis.com/icon?family=Material+Icons',
+	'js/materialize.min.js'
+];
+
+self.addEventListener('install', e => {
+
+	const cacheStatic = caches.open( STATIC_CACHE ).then(cache => {
+		cache.addAll(APP_SHELL);
+	});
+
+	const cacheInmut= caches.open( INMUTABLE_CACHE ).then(cache => {
+		cache.addAll(APP_SHELL_INMUT);
+	});
+
+	e.waitUntil( Promise.all([{cacheStatic,cacheInmut}]) );
+
+});
+
+self.addEventListener('activate', e => {
+
+	const respuesta = caches.keys().then(keys => {
+
+		keys.forEach(key =>{ 
+			if(key !== STATIC_CACHE && key.includes('static') ){
+				return caches.delete(key);
+			}
+		});
+	});
+
+	e.waitUntil(respuesta);
+});
+
+
+self.addEventListener('fetch', e => {
+
+	const respuesta = caches.match(e.request).then( res => {
+		if(res){
+			return res;
+
+		}else{
+			return fetch(e.request).then( newRes => {
+				return actualizaCacheDinamico(DYNAMIC_CACHE, e.request, newRes);
+			});
+				
+		}
+	});
+
+
+	e.respondWith(respuesta);
+
+});
