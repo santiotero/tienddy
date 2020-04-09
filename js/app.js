5e08d73baeecafa72991f184da466bbd51799b17
Santi Otero <poyovcp@gmail.com>
2020-04-09 19:31:36 -0300

    mejora app pwa android 2

--- js/app.js
+++ js/app.js
@@ -15,6 +15,7 @@ if(navigator.serviceWorker){
 var usuario = {};
 var tienda = {};
 var carrito = {};
+
 window.onload = function() {          
 
           pasosCompra('usuario');
@@ -26,6 +27,26 @@ window.onload = function() {
           obj.addEventListener("keydown", function(event){
             buscar(event);
           });
+
+          let deferredPrompt;
+          window.addEventListener('beforeinstallprompt', (e) => {
+            // Prevent Chrome 67 and earlier from automatically showing the prompt
+            e.preventDefault();
+            // Stash the event so it can be triggered later.
+            deferredPrompt = e;
+          });
+          // Show the prompt
+          deferredPrompt.prompt();
+          // Wait for the user to respond to the prompt
+          deferredPrompt.userChoice
+            .then((choiceResult) => {
+              if (choiceResult.outcome === 'accepted') {
+                console.log('User accepted the A2HS prompt');
+              } else {
+                console.log('User dismissed the A2HS prompt');
+              }
+              deferredPrompt = null;
+            });
 
 };
 
