048df025a2f95ccfcf69da7a5b893ab992816b0e
Santi Otero <poyovcp@gmail.com>
2020-04-09 17:57:59 -0300

    app pwa android 4

--- js/sw_util.js
+++ js/sw_util.js
@@ -4,7 +4,7 @@ function actualizaCacheDinamico(dynCache, req, res){
 
 	if( res.ok ){
 
-		caches.open(dynCache).then( cache => {
+		return caches.open(dynCache).then( cache => {
 
 			cache.put(req, res.clone());
 			return res.clone();
