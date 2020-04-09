3572523fbd78cf6778177ad28eb408bcfb812651
Santi Otero <poyovcp@gmail.com>
2020-04-09 17:43:41 -0300

    app pwa android 1

--- sw.js
+++ sw.js
@@ -67,7 +67,6 @@ self.addEventListener('fetch', e => {
 	const respuesta = caches.match(e.request).then( res => {
 		if(res){
 			return res;
-
 		}else{
 			return fetch(e.request).then( newRes => {
 				return actualizaCacheDinamico(DYNAMIC_CACHE, e.request, newRes);
