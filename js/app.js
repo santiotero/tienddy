b100588728ec93ad82cc65b82ed51bcea27eb196
Santi Otero <poyovcp@gmail.com>
2020-04-11 12:04:51 -0300

    correccion js

--- js/app.js
+++ js/app.js
@@ -9,7 +9,7 @@ if(navigator.serviceWorker){
    }
 
    navigator.serviceWorker.register(urlSw);
-   
+    
 }
 
 var usuario = {};
--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 importScripts('js/sw_util.js'); 
 
   
-const STATIC_CACHE = 'static-v5';
-const DYNAMIC_CACHE = 'dynamic-v5';
+const STATIC_CACHE = 'static-v6';
+const DYNAMIC_CACHE = 'dynamic-v6';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
 const APP_SHELL = [
