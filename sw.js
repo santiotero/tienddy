b54ac6ecaed4b548fbc2dc2ffd9ae93e42c5aded
Santi Otero <poyovcp@gmail.com>
2020-04-11 16:24:11 -0300

    mejoras fetch y sw 10.3

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@ const STATIC_CACHE = 'static-v9.3';
 const DYNAMIC_CACHE = 'dynamic-v9.4';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
-const APP_SHELL = [
+const APP_SHELL =  [
 	//'/',   
 	'index.html',
 	'img/favicon.png',
