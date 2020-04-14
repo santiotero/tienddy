e1bd3cdb1a470a55ddcb6ccb64aa99bb30a6850e
Santi Otero <poyovcp@gmail.com>
2020-04-14 20:35:22 -0300

    mapa seleccion tienda v1.8

--- sw_t2.js
+++ sw_t2.js
@@ -2,8 +2,8 @@ importScripts('js/sw_util_t2.js');
 
 //
  
-const STATIC_CACHE = 'static-v1.3';
-const DYNAMIC_CACHE = 'dynamic-v1.3';
+const STATIC_CACHE = 'static-v1.3.1';
+const DYNAMIC_CACHE = 'dynamic-v1.3.1';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
 const APP_SHELL =  [
