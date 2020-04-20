25303bc5fde2f3dadc8957eb2414d5c45eb8f2fe
exa85897 <santiago.otero@claro.com.ar>
2020-04-20 20:19:07 -0300

    validacion samsung 1.0.7

--- sw_t2.js
+++ sw_t2.js
@@ -68,12 +68,3 @@ self.addEventListener('activate', e => {
 self.addEventListener('fetch', function(event) {
   event.respondWith(fetch(event.request));
 });
-
-/*  
-self.addEventListener('fetch', e => {
-		return fetch(e.request).then( newRes => {
-				return actualizaCacheDinamico(DYNAMIC_CACHE, e.request, newRes);
-		});				
-	e.respondWith(respuesta); 
-});
-*/
